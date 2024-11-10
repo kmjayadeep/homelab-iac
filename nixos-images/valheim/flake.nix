a52e8b6d00a450c54b6a4e8918e3b44e4e14f5c3
Jayadeep KM <kmjayadeep@gmail.com>
2024-11-10 21:10:48 +0100

    add tailscale in valheim
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nixos-images/valheim/flake.nix
+++ nixos-images/valheim/flake.nix
@@ -16,6 +16,7 @@
           ./modules/user.nix
           ./modules/valheim-server.nix
           ./modules/backup.nix
+          ./modules/tailscale.nix
           ./hosts/fireland.nix
         ];
       };
