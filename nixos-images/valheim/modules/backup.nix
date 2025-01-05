a69b1c85be6ebf5caf479c5bfdab414c0acccb8c
Jayadeep KM <kmjayadeep@gmail.com>
2025-01-05 12:29:20 +0100

    update uptime script for valheim
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nixos-images/valheim/modules/backup.nix
+++ nixos-images/valheim/modules/backup.nix
@@ -20,7 +20,8 @@
       ${pkgs.restic}/bin/restic backup /var/lib/valheim/.config/unity3d/IronGate/Valheim/worlds_local --retry-lock 10m
       ${pkgs.restic}/bin/restic forget --keep-monthly 12 --keep-last 10 --keep-daily 10 --prune
       ${pkgs.restic}/bin/restic snapshots
-      ${pkgs.curl}/bin/curl ''${UPTIME_URL}
+      ${pkgs.systemd}/bin/systemctl is-active --quiet valheim && ${pkgs.curl}/bin/curl ''${UPTIME_URL}
+      ${pkgs.coreutils}/bin/echo "Backup completed successfully & Posted uptime status"
     '';
   };
 
