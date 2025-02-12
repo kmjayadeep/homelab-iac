43d1a4b1607693b9dba3fefc749083626a405779
Jayadeep KM <kmjayadeep@gmail.com>
2025-02-12 18:20:43 +0100

    gh runner changes
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ nixos-images/github-runner/README.md
@@ -0,0 +1,7 @@
+# Github runner
+
+# Token
+create token here:
+
+https://github.com/kmjayadeep/homelab-iac/settings/actions/runners/new
+https://github.com/kmjayadeep/near-dns/settings/actions/runners/new
--- nixos-images/github-runner/modules/github-runner.nix
+++ nixos-images/github-runner/modules/github-runner.nix
@@ -1,17 +1,31 @@
 { config, lib, pkgs, ... }: {
 
-  environment.etc.github-pat.source = ../secrets/secret-pat;
+  environment.etc.github-pat-runner1.source = ../secrets/secret-runner1;
+  environment.etc.github-pat-runner2.source = ../secrets/secret-runner2;
 
   services.github-runners = {
-    runner = {
+    runner-homelab-iac = {
       enable = true;
-      name = "runner";
-      tokenFile = "/etc/github-pat";
+      name = "runner-homelab-iac";
+      tokenFile = "/etc/github-pat-runner1";
       url = "https://github.com/kmjayadeep/homelab-iac";
       extraLabels = [ "nixos" "cosmos" ];
       extraPackages = with pkgs; [
         busybox
         nodejs_22
+      ];
+    };
+    runner-near-dns = {
+      enable = false;
+      name = "runner-near-dns";
+      tokenFile = "/etc/github-pat-runner2";
+      url = "https://github.com/kmjayadeep/near-dns";
+      extraLabels = [ "nixos" "cosmos" ];
+      extraPackages = with pkgs; [
+        busybox
+        nodejs_22
+        curl
+        cargo
       ];
     };
   };
