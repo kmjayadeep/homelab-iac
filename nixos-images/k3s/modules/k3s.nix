bbc90204b79c4313c6eac3da04f07d54f3756c30
Jayadeep KM <kmjayadeep@gmail.com>
2025-05-14 11:56:19 +0200

    allow sysctl in k3s
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nixos-images/k3s/modules/k3s.nix
+++ nixos-images/k3s/modules/k3s.nix
@@ -6,6 +6,9 @@
     role = "agent";
     serverAddr = "https://192.168.1.40:6443";
     tokenFile = "/etc/k3s-token";
+    extraFlags = toString [
+      "--kubelet-arg=allowed-unsafe-sysctls=net.ipv6.*,net.ipv4.*"
+    ];
   };
 
   environment.etc.k3s-token.source = ../secrets/secret-k3s-token;
