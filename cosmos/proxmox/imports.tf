e388bfc0db0c955cd3d58fcab48b3e24e474722a
Jayadeep KM <kmjayadeep@gmail.com>
2025-08-29 21:52:41 +0200

    import all vms in mars
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- cosmos/proxmox/imports.tf
+++ cosmos/proxmox/imports.tf
@@ -10,3 +10,23 @@ import {
   to = module.coder.proxmox_vm_qemu.this
   id = "mars/qemu/203"
 }
+import {
+  to = module.chillyfries.proxmox_vm_qemu.this
+  id = "mars/qemu/207"
+}
+import {
+  to = module.fireland.proxmox_vm_qemu.this
+  id = "mars/qemu/201"
+}
+import {
+  to = module.jd-vm.proxmox_vm_qemu.this
+  id = "mars/qemu/204"
+}
+import {
+  to = module.odin.proxmox_vm_qemu.this
+  id = "mars/qemu/205"
+}
+import {
+  to = module.valkyrie.proxmox_vm_qemu.this
+  id = "mars/qemu/209"
+}
