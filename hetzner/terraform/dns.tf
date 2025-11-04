0b9da44e705cfd1d29ed25c91a977e1464c76df6
Jayadeep KM <kmjayadeep@gmail.com>
2025-11-04 05:49:57 +0100

    fava hetzner dns
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- hetzner/terraform/dns.tf
+++ hetzner/terraform/dns.tf
@@ -28,3 +28,13 @@ resource "cloudflare_dns_record" "wiki_aaaa" {
 
   comment = "Psuite Wiki hetzner IPv6 address - managed by Terraform"
 }
+
+resource "cloudflare_dns_record" "fava_aaaa" {
+  zone_id = var.cloudflare_zone_id
+  name    = "fava.hetzner.${var.dns_domain}"
+  content = hcloud_server.nova.ipv6_address
+  type    = "AAAA"
+  ttl     = 300
+
+  comment = "Psuite Fava hetzner IPv6 address - managed by Terraform"
+}
