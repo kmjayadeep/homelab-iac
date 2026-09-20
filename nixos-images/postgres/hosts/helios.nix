3ae10be01769714a67e255dc7f3dfbe75d1c2345
Jayadeep KM <kmjayadeep@gmail.com>
2026-09-20 18:30:36 +0200

    fix(postgres): route wallabag pod traffic
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nixos-images/postgres/hosts/helios.nix
+++ nixos-images/postgres/hosts/helios.nix
@@ -3,10 +3,17 @@
   networking = {
     hostName = "helios";
     interfaces.ens18 = {
-      ipv4.addresses = [{
-        address = "192.168.1.77";
-        prefixLength = 24;
-      }];
+      ipv4 = {
+        addresses = [{
+          address = "192.168.1.77";
+          prefixLength = 24;
+        }];
+        routes = [{
+          address = "10.42.1.0";
+          prefixLength = 24;
+          via = "192.168.1.79";
+        }];
+      };
       ipv6.addresses = [{
         address = "fe80::77";
         prefixLength = 64;
